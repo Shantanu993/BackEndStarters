a133ad884a4c0d0bf285fc2c8e58f6e049e3b0ec
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-09 01:32:57 +0530

    async function wrapper using try catch

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+
+// USING TRY CATCH
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+const asyncHandler = (fn) => async (req,res,next) => {
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
