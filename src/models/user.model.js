fffcdc472197ef190bb5d591bd77e028e93c888a
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-09 22:45:34 +0530

    created basic video schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -33,7 +33,7 @@ const userSchema = new Schema(
     watchHistory: [
         {
         type: Schema.Types.ObjectId,
-        ref: "videos"
+        ref: "Videos"
         }
     ],
     password: {
