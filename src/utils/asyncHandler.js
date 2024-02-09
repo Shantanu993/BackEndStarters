ffe27fd254d031796fc6009a2516bbbeaa84efb5
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-09 16:13:47 +0530

    async function wrapper using promises

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,16 @@
+// Using Promises
+
+const asyncHandler = (requestHandler) => {
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
 
 // USING TRY CATCH
 
@@ -5,13 +18,13 @@
 // const asyncHandler = (func) => () => {}
 // const asyncHandler = (func) => async () => {}
 
-const asyncHandler = (fn) => async (req,res,next) => {
-    try {
-        await fn(req,res,next)
-    } catch (error) {
-        res.status(err.code || 500).json({
-            success: false,
-            message: err.message
-        })
-    }
-}
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
