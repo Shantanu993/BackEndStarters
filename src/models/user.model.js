277e9e935d1df5d99d306f0a86499085909b0a81
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-09 23:16:31 +0530

    add password encrytion logic in user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = new Schema(
     {
@@ -43,9 +45,16 @@ const userSchema = new Schema(
     refreshToken: {
         type: String
     }
-
-
 }
 ,{timestamps: true,})
+
+
+// password encryption logic
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next()
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
 
 export const User = mongoose.model("User", userSchema)
