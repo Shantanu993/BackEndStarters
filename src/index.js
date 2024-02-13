113e121ab611a1e5ec9c5d28010133e0cb6a9f2f
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-13 10:09:20 +0530

    🐛 Bug Fixes
    fix bugs in various files

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
 // require('dotenv').config({path: './env'})
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 // Professional Approach
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
 
 // OR dotenv.config()
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err)=>{
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("Error connecting to db", err);
-}
-)
-
-
-
+  });
 
 /*
  // This is one approach to connect to the database
