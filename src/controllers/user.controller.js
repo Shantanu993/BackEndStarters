60f0891b556e8529bb26d4415b795ae7599c0474
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-25 14:47:48 +0530

    algorithm for user registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,4 +6,16 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 });
 
+/*
+1. get details from frontend
+2. validations - not empty, etc.
+3. check if user already exists: username, email
+4. check for images, check for avatar
+5. upload them to cloudinary: avatar
+6. create user object- create entry in db
+7. remove password and refresh token field from response
+8. check for user creation
+9. return response
+ */
+
 export { registerUser };
