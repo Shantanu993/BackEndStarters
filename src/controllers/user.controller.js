9e3afccb670d197d8fda4214d2827d4d922294c7
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-25 15:27:10 +0530

    setup basic validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({
@@ -20,5 +21,17 @@ const registerUser = asyncHandler(async (req, res) => {
 
 const { fullName, email, username, password } = req.body;
 console.log("email: ", email);
+
+// if(fullName === ""){
+// 	throw new ApiError(400, "fullname is required")
+// }
+
+// Professional Way to check for all the fields
+
+if (
+  [fullName, email, username, password].some((field) => field?.trim() === "")
+) {
+  throw new ApiError(400, "All fields are required");
+}
 
 export { registerUser };
