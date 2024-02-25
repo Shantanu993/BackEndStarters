6935e78fa44b0e27381433d8d4b737f8d5448927
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-25 15:12:21 +0530

    add image upload functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,4 +18,7 @@ const registerUser = asyncHandler(async (req, res) => {
 9. return response
  */
 
+const { fullName, email, username, password } = req.body;
+console.log("email: ", email);
+
 export { registerUser };
