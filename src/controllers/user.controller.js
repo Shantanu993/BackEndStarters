40483b69dc8f6a65b1b65cb0a9a9e66e07d3b50a
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-04 18:45:20 +0530

    🐛 Fixed user controller bug
    ]

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,9 +8,8 @@ const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: "ok",
   });
-});
 
-/*
+  /*
 1. get details from frontend
 2. validations - not empty, etc.
 3. check if user already exists: username, email
@@ -22,64 +21,65 @@ const registerUser = asyncHandler(async (req, res) => {
 9. return response
  */
 
-const { fullName, email, username, password } = req.body;
-console.log("email: ", email);
+  const { fullName, email, username, password } = req.body;
+  console.log("email: ", email);
 
-// if(fullName === ""){
-// 	throw new ApiError(400, "fullname is required")
-// }
+  // if(fullName === ""){
+  // 	throw new ApiError(400, "fullname is required")
+  // }
 
-// Professional Way to check for all the fields
+  // Professional Way to check for all the fields
 
-if (
-  [fullName, email, username, password].some((field) => field?.trim() === "")
-) {
-  throw new ApiError(400, "All fields are required");
-}
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-const existedUser = User.findOne({
-  $or: [{ username }, { email }],
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  // middlewares add more fields in the req, in this case multer adds files
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
-
-if (existedUser) {
-  throw new ApiError(409, "User with email or username already exists");
-}
-
-// middlewares add more fields in the req, in this case multer adds files
-
-const avatarLocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
-if (!avatarLocalPath) {
-  throw new ApiError(400, "Avatar file is required");
-}
-
-const avatar = await uploadOnCloudinary(avatarLocalPath);
-const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-
-if (!avatar) {
-  throw new ApiError(400, "Avatar file is required");
-}
-
-const user = await User.create({
-  fullName,
-  avatar: avatar.url,
-  coverImage: coverImage?.url || "",
-  email,
-  password,
-  username: username.toLowerCase(),
-});
-
-const createdUser = await User.findById(user._id).select(
-  "-password -refreshToken"
-);
-
-if (!createdUser) {
-  throw new ApiError(500, "Something went wrong while registering the user");
-}
-
-return res
-  .status(201)
-  .json(new ApiResponse(200, createdUser, "User registered successfully"));
 
 export { registerUser };
