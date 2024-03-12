0218f2fc50b233e48fcf40de5b85e3aee70f7d7e
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-12 23:47:52 +0530

    get user with selected fields

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -142,6 +142,15 @@ const loginUser = asyncHandler(async (req, res) => {
   const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
     user._id
   );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
 });
 
 export { registerUser, loginUser };
